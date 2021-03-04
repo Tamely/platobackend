4e514692680c7b966b03374caac21b9edf70ccac
Tamely <joshua.audia@gmail.com>
2021-03-03 18:33:32 -0600

    small QOL

--- navbar.js
+++ navbar.js
@@ -2,6 +2,6 @@ function openNav() {
 	document.getElementById("side-bar").style.width = "50px";
   }
   
-  function closeNav() {
+function closeNav() {
 	document.getElementById("side-bar").style.width = "0px";
   }
